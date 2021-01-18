7a681b4fd5bd336fc43cf62e6f1e93a38c3bbfa0
Nick Palenchar <nickpal@nickpalenchar.com>
2021-01-18 10:22:11 -0500

    remove conole.log

--- boardUI.js
+++ boardUI.js
@@ -5,9 +5,7 @@ window.BoardUI = {
         /* Adds the ACTIVE_CELL_CLS to an element, and removing it from other elements which may currently
         hold it. In this way, the ACTIVE_CELL_CLS can only ever be on one cell at a time.
          */
-        console.log(event.target)
         let cell = event.target.classList.contains('cell') ? event.target : event.target.parentElement;
-        console.log('cell is ', cell);
         this._activeElement?.classList.remove(ACTIVE_CELL_CLS);
         cell.classList.add(ACTIVE_CELL_CLS);
         this._activeElement = cell;
